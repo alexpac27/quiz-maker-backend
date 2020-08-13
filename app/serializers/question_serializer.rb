5b55615a2e2416d0519ada65074561038db942de
alexpac27 <alexpac27@gmail.com>
2020-08-13 17:28:21 -0400

    removed serializer folder

--- app/serializers/question_serializer.rb
+++
@@ -1,3 +0,0 @@
-class QuestionSerializer < ActiveModel::Serializer
-  attributes :id, :category, :type, :difficulty, :question, :correct_answer, :incorrect_answer
-end
--- app/serializers/quiz_serializer.rb
+++
@@ -1,3 +0,0 @@
-class QuizSerializer < ActiveModel::Serializer
-  attributes :id, :name, :question_id, :user_id, :integer
-end
