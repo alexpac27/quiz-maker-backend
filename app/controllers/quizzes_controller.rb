3eabbabc7ed6d565b978d032b195be03d37ddd12
alexpac27 <alexpac27@gmail.com>
2020-08-13 19:38:53 -0400

    built out controllers

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -1,2 +1,29 @@
 class QuizzesController < ApplicationController
+
+    def index
+        @quizzes = Quiz.all 
+        render json: @quizzes, include: :question
+    end
+
+    def show
+        @quiz = Quiz.find_by(id:params[:id])
+        render json: @quiz, include: :question
+    end
+
+    def destroy 
+        quiz = Quiz.find_by(id: params[:id])
+        quiz.destroy
+    end 
+
+    def create
+        quiz = Quiz.create!(quiz_item_params)
+        render json: quiz, include: :question
+    end
+
+    private
+
+    def quiz_item_params
+        params.require(:quiz).permit(:user_id, :question_id,:name)
+    end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,19 +2,36 @@
 
 User.destroy_all
 Question.destroy_all
+Quiz.destroy_all
+
+
+# CREATING USERS
 
 alex = User.create(username: "alexo", password: "pass123")
 joseph = User.create(username: "josephy", password: "pass123")
 
-q1 = Question.create(category: "Geography", difficulty: "hard", question: "Which of these island countries is located in the Caribbean?", correct_answer: "Barbados", incorrect_answers: "Fiji,Maldives,Seychelles")
 
 
 # CREATING QUESTIONS
 
-# questions = JSON.parse(RestClient.get("https://opentdb.com/api.php?amount=20").body)["results"]
-# questions.each do |quest|
-#     Question.create!(category: quest["category"], difficulty: quest["difficulty"], question: quest["question"], correct_answer: quest["correct_answer"], incorrect_answers: (quest["incorrect_answers"].class == Array ? quest["incorrect_answers"].join(', '): quest["incorrect_answer"]))
-# end
+questions = JSON.parse(RestClient.get("https://opentdb.com/api.php?amount=20").body)["results"]
+questions.each do |quest|
+    Question.create!(category: quest["category"], difficulty: quest["difficulty"], question: quest["question"], correct_answer: quest["correct_answer"], incorrect_answers: (quest["incorrect_answers"].class == Array ? quest["incorrect_answers"].join(', '): quest["incorrect_answer"]))
+end
 
-# https://opentdb.com/api.php?amount=25&category={9}&difficulty={easy}&type=multiple
+# CREATING QUIZZES 
+
+Quiz.create(name:"Alex's History Quiz", user_id: 1, question_id: 1)
+Quiz.create(name:"Alex's History Quiz", user_id: 1, question_id: 2)
+Quiz.create(name:"Random Quiz", user_id: 1, question_id: 3)
+Quiz.create(name:"Random Quiz", user_id: 1, question_id: 4)
+Quiz.create(name:"Random Quiz", user_id: 1, question_id: 5)
+
+Quiz.create(name:"Joseph's Awesome Quiz", user_id: 2, question_id: 12)
+Quiz.create(name:"Joseph's Awesome Quiz", user_id: 2, question_id: 13)
+Quiz.create(name:"Joseph's Awesome Quiz", user_id: 2, question_id: 14)
+Quiz.create(name:"All About the Quiz", user_id: 2, question_id: 15)
+Quiz.create(name:"All About the Quiz", user_id: 2, question_id: 16)
+
+# https://opentdb.com/api.php?amount=25&category=9&difficulty=easy&type=multiple
 # https://opentdb.com/api.php?amount=10&category=9&difficulty=easy
